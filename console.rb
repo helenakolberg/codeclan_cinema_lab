b4aaf05fcf5b66a29ecf67c391179665086b281d
Helena <inger.helena@gmail.com>
2020-07-05 10:40:29 +0100

    added ticket objects

--- console.rb
+++ console.rb
@@ -49,6 +49,17 @@ ticket1 = Ticket.new({
 })
 ticket1.save()
 
+ticket2 = Ticket.new({
+    'customer_id' => customer2.id,
+    'film_id' => film1.id
+})
+ticket2.save()
+
+ticket3 = Ticket.new({
+    'customer_id' => customer2.id,
+    'film_id' => film3.id
+})
+ticket3.save()
 
 
 
